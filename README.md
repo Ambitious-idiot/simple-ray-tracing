bc4bd22815d9b6d3abdad76d512c8ae73ffd01d6
YHSong <1308293139@qq.com>
2022-08-08 15:10:40 +0800

    first output

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ray Tracing in One Weekend Notes
+## Output a PPM image
+Write the rgb triplets to the file with the redirection '>'
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 # include <iostream>
 
 int main() {
-    const int img_width = 256
+    const int img_width = 256;
+    const int img_height = 256;
+    std::cout << "P3\n" << img_width << ' '
+        << img_height << "\n255\n";
+    for (int j = img_height - 1; j >= 0; --j) {
+        for (int i = 0; i < img_width; ++i) {
+            auto r = double(i) / (img_width - 1);
+            auto g = double(j) / (img_height - 1);
+            auto b = 0.25;
+            int ir = static_cast<int>(255.9999 * r);
+            int ig = static_cast<int>(255.9999 * g);
+            int ib = static_cast<int>(255.9999 * b);
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }

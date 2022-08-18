733b4877d3fbd05753a23266f207562f12f54f38
YHSong <1308293139@qq.com>
2022-08-18 11:20:06 +0800

    add diffuse light

--- include/material/Material.h
+++ include/material/Material.h
@@ -2,6 +2,7 @@
 #define _MATERIAL_H
 
 #include "texture.h"
+#include "vec3.h"
 
 struct HitRecord;
 
@@ -10,6 +11,10 @@ class Material {
         virtual bool scatter(
             const Ray& r_in, const HitRecord& rec, Color& attenuation, Ray& scattered
         ) const = 0;
+
+        virtual Color emit(double u, double v, const Point3& p) const {
+            return Color(0, 0, 0);
+        }
 };
 
 #endif
---
+++ include/material/diffuse_light.h
@@ -0,0 +1,23 @@
+#ifndef DIFFUSE_LIGHT_H
+#define DIFFUSE_LIGHT_H
+
+#include "Material.h"
+#include "vec3.h"
+
+class DiffuseLight: public Material {
+    shared_ptr<Texture> texture;
+
+    public:
+        DiffuseLight(const shared_ptr<Texture>& t): texture(t) {}
+        DiffuseLight(Color c): texture(make_shared<SolidColor>(c)) {}
+
+        virtual bool scatter(
+            const Ray& r_in, const HitRecord& rec, Color& attenuation, Ray& scattered
+        ) const override { return false; }
+
+        virtual Color emit(double u, double v, const Point3& p) const override {
+            return texture->value(u, v, p);
+        }
+};
+
+#endif
